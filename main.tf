53f2ebc70ac0b63e27cd4cdf21ec9168420406b7
nhaila-abdessamad <abdessamad.nhaila@gmail.com>
2022-08-09 12:09:44 +0100

    up

--- main.tf
+++ main.tf
@@ -1,22 +1,21 @@
 
 
-resource "aws_instance" "my-machines" {
+resource "aws_instance" "my-test-machine" {
   # Creates x identical aws ec2 instances
-  count = 1
+  count = 1  
   
   # All x instances will have the same ami and instance_type
   ami = lookup(var.ec2_ami,var.region) 
   instance_type = var.instance_type # 
-
   #subnet_id = "subnet-027ddfd1ac9b5daa1"
   key_name = "MULTIPLE"
-
   #user_data = "${file("startup.sh")}"
+
   root_block_device {
-    volume_size = 10 # in GB <<----- I increased this!
+    volume_size = 25 # in GB <<----- I increased this!
     volume_type = "gp3"
   }
-
+  
   connection {
     type = "ssh"
     user = "ec2-user"
@@ -42,12 +41,13 @@ resource "aws_instance" "my-machines" {
     # starting with the distinct index number 0 and corresponding to this instance.
     Name = "NAILAS-${count.index}"
   }
+  
 
 }
 
 resource "aws_eip" "elastic_ip" {
-  count    = length(aws_instance.my-machines)
+  count    = length(aws_instance.my-test-machine)
   #name = "eip-${var.my-machines}"
-  instance = aws_instance.my-machines[count.index].id
+  instance = aws_instance.my-test-machine[count.index].id
   vpc = true
   }
--- startup.sh
+++ startup.sh
@@ -3,3 +3,16 @@
 sudo echo "export PS1=\"\[\e[32m\][\[\e[m\]\[\e[31m\]\u\[\e[m\]\[\e[33m\]@\[\e[m\]\[\e[32m\]\h\[\e[m\]:\[\e[36m\]\w\[\e[m\]\[\e[32m\]]\\$\[\e[m\] \" " > .bashrc  
 sudo . .bashrc
 
+
+sudo yum update -y
+sudo yum install -y git
+
+
+#var="$(curl http://checkip.amazonaws.com)"
+
+#sudo sed -i "s/localhost/$var/g" /home/ec2-user/Full-Text-Search-App-ES/public/app.js
+
+# docker-compose up -d --build
+# docker exec gs-api "node" "server/load_data.js"
+
+
